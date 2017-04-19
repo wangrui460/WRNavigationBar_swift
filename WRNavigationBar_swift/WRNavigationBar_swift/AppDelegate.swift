12bf167645b242289bfd9c411c106e916a4f5a66
wangrui460 <wangruidev@gmail.com>
2017-04-19 15:17:13 +0800

    update DemoListController

--- WRNavigationBar_swift/WRNavigationBar_swift/AppDelegate.swift
+++ WRNavigationBar_swift/WRNavigationBar_swift/AppDelegate.swift
@@ -7,7 +7,14 @@
 //
 
 import UIKit
+
 let MainNavBarColor = UIColor.init(red: 0/255.0, green: 175/255.0, blue: 240/255.0, alpha: 1)
+let kScreenWidth = UIScreen.main.bounds.width
+let kScreenHeight = UIScreen.main.bounds.height
+let kTabBarHeight = 49
+let kNavBarBottom = 64
+let kNavBarHeight = 44
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
--- WRNavigationBar_swift/WRNavigationBar_swift/Demos/DemoListController.swift
+++ WRNavigationBar_swift/WRNavigationBar_swift/Demos/DemoListController.swift
@@ -8,12 +8,63 @@
 
 import UIKit
 
-class DemoListController: UIViewController {
-
+class DemoListController: UIViewController
+{
+    lazy var tableView:UITableView = UITableView(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: self.view.bounds.height), style: .plain)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.green
-        self.title = "DemoList"
+        title = "DemoList"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 }
+
+
+// MARK: - tableView delegate / dataSource
+extension DemoListController: UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: nil)
+        var str:String? = nil
+        switch indexPath.row {
+        case 0:
+            str = "基本";
+        case 1:
+            str = "超过临界点移动导航栏";
+        case 2:
+            str = "超过临界点多少，移动导航栏多少(不会超过44)";
+        case 3:
+            str = "类似qq应用空间效果";
+        case 4:
+            str = "类似QQ空间效果";
+        default:
+            str = ""
+        }
+        cell.textLabel?.text = str
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 15)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+
+
+
